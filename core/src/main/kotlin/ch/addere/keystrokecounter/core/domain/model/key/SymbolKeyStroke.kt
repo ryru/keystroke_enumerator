be71d8ba8b69f934fe7d83703029972fc1e1c6d7
Pascal Knecht <pascal@addere.ch>
2023-03-21 12:18:20 +0100

    Add virtual key domain model classes

---
+++ core/src/main/kotlin/ch/addere/keystrokecounter/core/domain/model/key/SymbolKeyStroke.kt
@@ -0,0 +1,16 @@
+package ch.addere.keystrokecounter.core.domain.model.key
+
+import ch.addere.keystrokecounter.core.domain.model.Symbol
+
+/**
+ * A symbol maps to a list of keystrokes.
+ */
+data class SymbolKeyStroke(val symbol: Symbol, val virtualKeyStrokes: List<VirtualKey>) {
+
+    /**
+     * Amount of keystrokes to generate this symbol.
+     */
+    fun keyStrokeCount(): Int {
+        return virtualKeyStrokes.size
+    }
+}

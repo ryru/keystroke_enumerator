de3672fd6a73b169f2da08d27e54a4bdd6329fd8
Pascal Knecht <pascal@addere.ch>
2023-04-02 15:14:43 +0200

    Add test coverage verification with JaCoCo in core module

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    jacoco
     kotlin("jvm") version "1.8.0"
 }
 
@@ -25,5 +26,23 @@ kotlin {
     jvmToolchain {
         this.languageVersion.set(JavaLanguageVersion.of(19))
         vendor.set(JvmVendorSpec.ADOPTIUM)
+    }
+}
+
+tasks.named("check") {
+    dependsOn("jacocoTestCoverageVerification")
+}
+
+tasks.named("jacocoTestReport") {
+    dependsOn(testing.suites.named("test"))
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            limit {
+                minimum = "0.8".toBigDecimal()
+            }
+        }
     }
 }

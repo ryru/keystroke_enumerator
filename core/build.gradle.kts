6af528f0ad11275922667f9a21ffe4f796244c77
Pascal Knecht <pascal@addere.ch>
2023-04-02 15:25:11 +0200

    Use kotlin plugin from gradle versions catalog

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,6 +1,7 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
+    alias(libs.plugins.kotlin.jvm)
     jacoco
-    kotlin("jvm") version "1.8.0"
 }
 
 group = "ch.addere"
